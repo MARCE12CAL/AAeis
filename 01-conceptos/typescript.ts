3517b21bc920118c20c7f497e9f8c8ece2779a74
MARCE12CAL <calvachemarli@gmail.com>
2024-09-20 20:35:06 -0500

    as4

---
+++ 01-conceptos/typescript.ts
@@ -0,0 +1,27 @@
+let variable:string ="hola mundo";
+
+console.log(variable);
+
+let variable2:number=1000;
+let arreglo:number[]=[1,2,3,4,5];
+type Persona={
+    nombre:string;
+    edad:number;
+    isAlive:boolean;
+    observations:{
+        status:boolean;
+
+    },
+
+
+};
+
+let objeto:Persona={
+    nombre:"Juan",
+    edad:30,
+    isAlive:true,
+   observations:{
+    status:true,
+   },
+
+};
